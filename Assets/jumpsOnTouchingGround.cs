f0de5f97327d84859cb98046f1f2d8e58010c1b6
Gonzako <al341522@uji.es>
2020-01-25 22:23:29 +0100

    added bouncy boy

---
+++ Assets/jumpsOnTouchingGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpsOnTouchingGround : MonoBehaviour
+{
+    private const string groundLayerName = "Ground";
+    Rigidbody rb;
+    public float ForceToAdd = 50;
+    public float timeBetweenJumps = 10;
+    private float timerBetweenJumps = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.GetMask(groundLayerName))
+        {
+            rb.AddForce(0, ForceToAdd / Time.fixedDeltaTime, 0, ForceMode.Impulse);
+            timerBetweenJumps = Time.time + timeBetweenJumps;
+        }
+    }
+}

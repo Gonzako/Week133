5afbd1daebd686e671a166ed5c4bf7c8de42bf72
Gonzako <al341522@uji.es>
2020-01-25 23:03:44 +0100

    work on lightnign project settings + added killbox code

---
+++ Assets/killPlayerOnTouch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killPlayerOnTouch : MonoBehaviour
+{
+    private const string playerLayer = "PlayerLayer";
+    public static event Action onPlayerKill;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer(playerLayer))
+        {
+            onPlayerKill?.Invoke();
+        }
+    }
+}

313429870139852d27b532f74ccf1d60c24b30e5
Gonzako <al341522@uji.es>
2020-01-26 21:00:05 +0100

    added level reload on player touch

---
+++ Assets/RestartLevelOnPlayerDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevelOnPlayerDeath : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        killPlayerOnTouch.onPlayerKill += restartLevel;
+    }
+
+    void restartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+    }
+
+    private void OnDisable()
+    {
+        killPlayerOnTouch.onPlayerKill -= restartLevel;
+    }
+}

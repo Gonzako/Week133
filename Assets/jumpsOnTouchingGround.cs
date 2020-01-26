8d14085d37b3169b52765b12c38f3e6ff56bc4a6
Gonzako <al341522@uji.es>
2020-01-26 22:16:57 +0100

    added wait time bettween jumps and jump + landing callbacks

--- Assets/jumpsOnTouchingGround.cs
+++ Assets/jumpsOnTouchingGround.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class jumpsOnTouchingGround : MonoBehaviour
 {
+    public static event Action<GameObject> onBlockLand;
+    public static event Action<GameObject> onBlockJump;
+
+
+
     private const string groundLayerName = "Ground";
     Rigidbody rb;
     public float ForceToAdd = 50;
-    public float timeBetweenJumps = 10;
+    public float timeOnGround = 3;
     private float timerBetweenJumps = 0.1f;
     // Start is called before the first frame update
     void Start()
@@ -15,13 +21,21 @@ public class jumpsOnTouchingGround : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    IEnumerator jumpRoutine()
+    {
+        yield return new WaitForSeconds(timeOnGround);
+        rb.AddForce(0, ForceToAdd / Time.fixedDeltaTime, 0, ForceMode.Impulse);
+        onBlockJump?.Invoke(this.gameObject);
+    }
+
+
     // Update is called once per frame
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer(groundLayerName))
         {
-            rb.AddForce(0, ForceToAdd / Time.fixedDeltaTime, 0, ForceMode.Impulse);
-            timerBetweenJumps = Time.time + timeBetweenJumps;
+            onBlockLand?.Invoke(this.gameObject);
+            StartCoroutine(jumpRoutine());
         }
     }
 }

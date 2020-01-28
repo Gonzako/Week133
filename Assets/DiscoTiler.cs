9134fe0bd84caf786929aeb6ca1c599d0911b020
Gonzako <al341522@uji.es>
2020-01-28 16:13:03 +0100

    added disco tiler (have to work on animations)

--- Assets/DiscoTiler.cs
+++ Assets/DiscoTiler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode, RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(BoxCollider))]
 public class DiscoTiler : MonoBehaviour
 {
     private BoxCollider col;
@@ -10,7 +10,7 @@ public class DiscoTiler : MonoBehaviour
     public int Rows = 2, Colums = 2;
     public List<Color> colorsToLoopThrough;
 
-    private List<GameObject> childrenGameObject;
+    private List<MeshRenderer> childrenMeshRenderers = new List<MeshRenderer>();
     private GonzakoUtils.DataStructures.Pool<GameObject> pool;
 
     private Vector3 gridPos;
@@ -21,10 +21,13 @@ public class DiscoTiler : MonoBehaviour
     void Start()
     {
         col = GetComponent<BoxCollider>();
-        pool = new GonzakoUtils.DataStructures.Pool<GameObject>(Rows * Colums + 5, baseTile, this.transform);
+        pool = new GonzakoUtils.DataStructures.Pool<GameObject>(Rows * Colums, baseTile, this.transform);
 
-
-
+        fillCollider(Rows, Colums);
+        foreach(MeshRenderer n in childrenMeshRenderers)
+        {
+            setRandomColorOnlist(n);
+        }
     }
 
     private void fillCollider(int rows, int colums)
@@ -35,82 +38,47 @@ public class DiscoTiler : MonoBehaviour
 
         
 
-        oddCheck.x = gridPos.x % 2 == 1 ? 0f : 0.5f;
-        oddCheck.y = gridPos.y % 2 == 1 ? 0f : 0.5f;
-        oddCheck.z = gridPos.z % 2 == 1 ? 0f : 0.5f;
+        oddCheck.x = rows % 2 == 1 ? 0f : 0.5f;
+        oddCheck.y = 0;
+        oddCheck.z = colums % 2 == 1 ? 0f : 0.5f;
 
-        for (float i = 0; i < rows; i++)
+        int listIndex = 0;
+
+        for (int i = 0; i < rows; i++)
         {
-            for (float j = 0; j < colums; j++)
+            for (int j = 0; j < colums; j++)
             {
                 var newTile = pool.getNextObj();
                 newTile.transform.localScale = tileSize;
-                newTile.transform.position = new Vector3(,,);
+                newTile.transform.localPosition = new Vector3(
+                ((i - rows/2 + 1) * tileSize.x + col.center.x),
+                col.center.y,
+                ((j + 1f - colums/2) * tileSize.z + col.center.z));
+
+                childrenMeshRenderers.Add(newTile.GetComponentInChildren<MeshRenderer>());
+                newTile.SetActive(true);
             }
         }
+    }
+
+    private void setRandomMaterialColor(MeshRenderer renderer)
+    {
+        MaterialPropertyBlock newMatPropBlock = new MaterialPropertyBlock();
+        newMatPropBlock.SetColor("_Color", Random.ColorHSV());
+        renderer.SetPropertyBlock(newMatPropBlock);
+    }
+
+    private void setRandomColorOnlist(MeshRenderer renderer)
+    {
+        MaterialPropertyBlock newMatPropBlock = new MaterialPropertyBlock();
+        newMatPropBlock.SetColor("_Color", colorsToLoopThrough[Random.Range(0, colorsToLoopThrough.Count)]);
+        renderer.SetPropertyBlock(newMatPropBlock);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-}
-
-public class SubCollider
-{
-
-    public Transform cube;
-    GameObject[] cubes;
-    // Vector3
-    int gridX;
-    int gridY;
-    int gridZ;
-    // our box collider
-    BoxCollider bc;
-    // even or odd?
-    float rtnX;
-    float rtnY;
-    float rtnZ;
-
-    void Awake()
-    {  // find the box collider
-    
-        // compute if X, Y or Z scale are even or odd
-        if (gridX % 2 == 1)
-            rtnX = 0.0f;
-        else
-            rtnX = 0.5f;
-
-        if (gridY % 2 == 1)
-            rtnY = 0.0f;
-        else
-            rtnY = 0.5f;
-
-        if (gridZ % 2 == 1)
-            rtnZ = 0.0f;
-        else
-            rtnZ = 0.5f;
-    }
-
-    void Start()
-    {  // display for each integer/Vector3 inside the box collider a sub object
-        for (int x = 0; x < gridX; x = x + 1)
-        {
-            for (int y = 0; y < gridY; y = y + 1)
-            {
-                for (int z = 0; z < gridZ; z = z + 1)
-                    Instantiate(cube, new Vector3((transform.position.x + x - (Mathf.FloorToInt(gridX / 2)) + rtnX),
-                        (transform.position.y + y - (Mathf.FloorToInt(gridY / 2)) + rtnY),
-                        (transform.position.z + z - Mathf.FloorToInt(gridZ / 2)) + rtnZ), Quaternion.identity);
-            }
-        }
-        // hierarchy
-        foreach (GameObject cc in cubes)
-            cc.transform.parent = this.transform;
-        // disable main collider and script at end
-        bc.enabled = false;
-        this.enabled = false;
     }
 }
 

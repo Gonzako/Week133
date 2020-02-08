cb6ecdbdea0bd14f1308d21896e3f5e172b1ac5c
Gonzako <al341522@uji.es>
2020-02-08 22:00:55 +0100

    meh

---
+++ Assets/changeMaterialOffset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeMaterialOffset : MonoBehaviour
+{
+    public float speed = 2;
+    private Vector2 currentOffset;
+    private Vector2 rand;
+    private MeshRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rand = Random.insideUnitCircle + Vector2.one/3;
+        rand.Normalize();
+        renderer.material.SetTextureOffset("_MainTex", currentOffset + speed * rand* Time.deltaTime);
+        currentOffset +=  rand * speed * Time.deltaTime;
+    }
+}
